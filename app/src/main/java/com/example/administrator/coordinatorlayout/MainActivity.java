c0b60931c2b52015a31ffee76a4f799a99966dec
dengjiali0925 <dengjiali0925@sina.com>
2018-08-20 11:45:14 +0800

    第六次提交

--- app/src/main/java/com/example/administrator/coordinatorlayout/MainActivity.java
+++ app/src/main/java/com/example/administrator/coordinatorlayout/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         useConstraintLayout.setOnClickListener(this);
         useCoordinatorLayout.setOnClickListener(this);
-        useCoordinatorLayout.setOnClickListener(this);
     }
 
     @Override
