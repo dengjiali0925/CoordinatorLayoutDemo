12ca9d5e69e207b5f51fad925b97d298bf4721da
dengjiali0925 <dengjiali0925@sina.com>
2018-08-20 11:15:27 +0800

    Update MainActivity.java
    第四次提交

--- app/src/main/java/com/example/administrator/coordinatorlayout/MainActivity.java
+++ app/src/main/java/com/example/administrator/coordinatorlayout/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         useConstraintLayout = (Button) findViewById(R.id.use_constraint_layout);
         useCoordinatorLayout = (Button) findViewById(R.id.use_coordinator_layout);
-        useCoordinatorLayout = (Button) findViewById(R.id.use_coordinator_layout);
     }
 
     private void setListener()
